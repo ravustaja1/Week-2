9137019ba1f919c0e59250842ad8b2b2bf82a184
Taneli Solehmainen <taneli.solehmainen@dnainternet.net>
2024-11-11 15:30:33 +0200

    commit

---
+++ app.ts
@@ -0,0 +1,65 @@
+type TVehicle = {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+let vehicle: TVehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+}
+
+console.log(vehicle)
+
+interface IVehicle {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+interface ICar extends IVehicle {
+    bodyType: string,
+    wheelCount: number
+}
+
+interface IBoat extends IVehicle {
+    draft: number
+}
+
+interface IPlane extends IVehicle {
+    wingspan: number
+}
+
+let car: ICar = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+}
+
+let plane: IPlane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingspan: 65
+}
+
+let boat: IBoat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+}
+
+
+console.log(car)
+console.log(plane)
+console.log(boat)
---
+++ dist/app.js
@@ -0,0 +1,33 @@
+"use strict";
+let vehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+};
+console.log(vehicle);
+let car = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+};
+let plane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingspan: 65
+};
+let boat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+};
+console.log(car);
+console.log(plane);
+console.log(boat);
